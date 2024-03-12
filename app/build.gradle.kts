4ca63e6e05969059bc5680e7408b4ed8c2bea508
NLCaceres <NLCaceres21@Gmail.com>
2024-03-12 03:47:16 -0700

    Add ViewModel, Coroutines, StateFlow, and Mockito dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,11 @@ android {
 
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
   implementation("androidx.activity:activity-compose:1.8.2")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
   implementation(platform("androidx.compose:compose-bom:2024.02.02"))
   implementation("androidx.compose.ui:ui")
@@ -57,7 +60,16 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
 
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+  implementation("com.google.code.gson:gson:2.10.1")
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
   testImplementation("junit:junit:4.13.2")
+  testImplementation("androidx.test.ext:junit-ktx:1.1.5")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
+  testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
