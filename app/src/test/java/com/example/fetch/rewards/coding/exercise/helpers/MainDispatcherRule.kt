f09adddcb5861d9c8a08356e3e456c8b5a78546e
NLCaceres <NLCaceres21@Gmail.com>
2024-03-12 15:34:15 -0700

    Create Dispatch rule to override CoroutineDispatcher used during testing

---
+++ app/src/test/java/com/example/fetch/rewards/coding/exercise/helpers/MainDispatcherRule.kt
@@ -0,0 +1,17 @@
+package com.example.fetch.rewards.coding.exercise.helpers
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+// Reusable JUnit4 TestRule to override the Main Coroutine Dispatcher used during tests
+@ExperimentalCoroutinesApi
+class MainDispatcherRule(val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()): TestWatcher() {
+  override fun starting(description: Description) { Dispatchers.setMain(testDispatcher) }
+  override fun finished(description: Description) { Dispatchers.resetMain() }
+}
