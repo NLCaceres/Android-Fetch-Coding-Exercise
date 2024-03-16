57540e1691745d3ffd7d705ef88ecb18ff42c1d6
NLCaceres <NLCaceres21@Gmail.com>
2024-03-16 08:11:55 -0700

    Add simple Accept header to GET request

--- app/src/main/java/com/example/fetch/rewards/coding/exercise/data/ItemService.kt
+++ app/src/main/java/com/example/fetch/rewards/coding/exercise/data/ItemService.kt
@@ -2,9 +2,11 @@ package com.example.fetch.rewards.coding.exercise.data
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 // BaseURL = "https://fetch-hiring.s3.amazonaws.com/"
 interface ItemService {
+  @Headers("Accept: application/json,*/*;q=0.8")
   @GET("hiring.json")
   suspend fun getItems(): Response<List<Item>>
 }
