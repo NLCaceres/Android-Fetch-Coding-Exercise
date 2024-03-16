3c1ad7b1982eb4469dab70144af23f9c5f0de78d
NLCaceres <NLCaceres21@Gmail.com>
2024-03-16 10:52:23 -0700

    Add AppContainer composable with ItemListView

--- app/src/main/java/com/example/fetch/rewards/coding/exercise/MainActivity.kt
+++ app/src/main/java/com/example/fetch/rewards/coding/exercise/MainActivity.kt
@@ -3,53 +3,23 @@ package com.example.fetch.rewards.coding.exercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.fetch.rewards.coding.exercise.ui.theme.FetchRewardsCodingExerciseTheme
+import com.example.fetch.rewards.coding.exercise.views.itemList.AppContainer
 import com.example.fetch.rewards.coding.exercise.views.itemList.ItemListView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       FetchRewardsCodingExerciseTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Scaffold(topBar = {
-            CenterAlignedTopAppBar(
-              title = { Text("Item List", fontSize = 40.sp, fontWeight = FontWeight.Bold) },
-              colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary, titleContentColor = MaterialTheme.colorScheme.onPrimary)
-            )
-          }) { innerPadding ->
-            ItemListView(Modifier.padding(innerPadding))
-          }
+        AppContainer { innerPadding ->
+          ItemListView(Modifier.padding(innerPadding))
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  FetchRewardsCodingExerciseTheme {
-    Greeting("Android")
   }
 }
