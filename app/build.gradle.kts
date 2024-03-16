228d3a656e5c99d6ca81b170a76f197379c349ab
NLCaceres <NLCaceres21@Gmail.com>
2024-03-15 20:00:38 -0700

    Add Hilt for lifecycle-aware dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("com.google.dagger.hilt.android")
+  id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -53,6 +55,8 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+  implementation("com.google.dagger:hilt-android:2.51")
+  kapt("com.google.dagger:hilt-compiler:2.51")
 
   implementation(platform("androidx.compose:compose-bom:2024.02.02"))
   implementation("androidx.compose.ui:ui")
@@ -78,4 +82,8 @@ dependencies {
 
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+kapt {
+  correctErrorTypes = true
 }
