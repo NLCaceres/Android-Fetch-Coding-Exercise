8fdccb1eef269587d731e10a30290befd17f38ce
NLCaceres <NLCaceres21@Gmail.com>
2024-03-16 10:10:00 -0700

    Setup App TopBar with Scaffold in MainActivity setContent

--- app/src/main/java/com/example/fetch/rewards/coding/exercise/MainActivity.kt
+++ app/src/main/java/com/example/fetch/rewards/coding/exercise/MainActivity.kt
@@ -4,24 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.fetch.rewards.coding.exercise.ui.theme.FetchRewardsCodingExerciseTheme
+import com.example.fetch.rewards.coding.exercise.views.itemList.ItemListView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       FetchRewardsCodingExerciseTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          Scaffold(topBar = {
+            CenterAlignedTopAppBar(
+              title = { Text("Item List", fontSize = 40.sp, fontWeight = FontWeight.Bold) },
+              colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary, titleContentColor = MaterialTheme.colorScheme.onPrimary)
+            )
+          }) { innerPadding ->
+            ItemListView(Modifier.padding(innerPadding))
+          }
         }
       }
     }
