a39f346e2d96826426ae38e1c5833432da8fb7d5
NLCaceres <NLCaceres21@Gmail.com>
2024-03-16 08:01:05 -0700

    Add AndroidEntryPoint and HiltApplication class for Hilt to grab onto

---
+++ app/src/main/java/com/example/fetch/rewards/coding/exercise/FetchRewardsCodingExerciseApplication.kt
@@ -0,0 +1,8 @@
+package com.example.fetch.rewards.coding.exercise
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/** Provides an entry point for Hilt to attach, so it can begin the injection process */
+@HiltAndroidApp
+class FetchRewardsCodingExerciseApplication: Application()
