92b0d54911bd9d9d868fda4b8649fd884c90a191
NLCaceres <NLCaceres21@Gmail.com>
2024-03-16 10:49:53 -0700

    Setup App container for Scaffold to display main content

---
+++ app/src/main/java/com/example/fetch/rewards/coding/exercise/views/itemList/AppContainer.kt
@@ -0,0 +1,40 @@
+package com.example.fetch.rewards.coding.exercise.views.itemList
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.fetch.rewards.coding.exercise.ui.theme.FetchRewardsCodingExerciseTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppContainer(content: @Composable (PaddingValues) -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Item List", fontSize = 40.sp, fontWeight = FontWeight.Bold) },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary, titleContentColor = MaterialTheme.colorScheme.onPrimary)
+            )
+        }) { innerPadding ->
+            content(innerPadding)
+        }
+    }
+}
+
+@Preview(widthDp = 400, heightDp = 800, showBackground = true, apiLevel = 33)
+@Composable
+fun HomeViewPreview() {
+    FetchRewardsCodingExerciseTheme {
+        AppContainer { innerPadding ->
+            Box(Modifier.fillMaxSize().padding(innerPadding)) {
+                Text("Hello World!")
+            }
+        }
+    }
+}
